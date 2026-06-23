460ec3220dfde188297fbed7203485d09aa3207e
Applebax <116964579+Applebax@users.noreply.github.com>
2026-06-23 15:49:48 -0500

    tagged all blood rutile items and blocks tagged all chartium items and blocks

--- src/main/java/com/peppamy/ascentexp/datagen/AscentExperimentBlockLootTableGenerator.java
+++ src/main/java/com/peppamy/ascentexp/datagen/AscentExperimentBlockLootTableGenerator.java
@@ -19,7 +19,15 @@ public class AscentExperimentBlockLootTableGenerator extends FabricBlockLootTabl
     @Override
     public void generate() {
         addDrop(AscentExperimentBlocks.SAPPHIRE_BLOCK);
+        addDrop(AscentExperimentBlocks.BLOOD_RUTILE_BLOCK);
+        addDrop(AscentExperimentBlocks.CHARTIUM_BLOCK);
+        addDrop(AscentExperimentBlocks.ASTER_STONE);
+        addDrop(AscentExperimentBlocks.CHARTIUM_BRICKS);
+        addDrop(AscentExperimentBlocks.RAW_CHARTIUM_BLOCK);
         this.addDrop(AscentExperimentBlocks.SAPPHIRE_ORE, block -> this.oreDrops(block, AscentExperimentItems.SAPPHIRE));
         this.addDrop(AscentExperimentBlocks.DEEPSLATE_SAPPHIRE_ORE, block -> this.oreDrops(block, AscentExperimentItems.SAPPHIRE));
+        this.addDrop(AscentExperimentBlocks.BLACKSTONE_RUTILE_ORE, block -> this.oreDrops(block, AscentExperimentItems.BLOOD_RUTILE));
+        this.addDrop(AscentExperimentBlocks.BASALT_RUTILE_ORE, block -> this.oreDrops(block, AscentExperimentItems.BLOOD_RUTILE));
+        this.addDrop(AscentExperimentBlocks.CHARTIUM_ORE, block -> this.oreDrops(block, AscentExperimentItems.RAW_CHARTIUM));
     }
 }
--- src/main/java/com/peppamy/ascentexp/datagen/AscentExperimentTagGenerators.java
+++ src/main/java/com/peppamy/ascentexp/datagen/AscentExperimentTagGenerators.java
@@ -24,12 +24,28 @@ public class AscentExperimentTagGenerators {
             getOrCreateTagBuilder(BlockTags.NEEDS_IRON_TOOL)
                     .add(AscentExperimentBlocks.SAPPHIRE_BLOCK)
                     .add(AscentExperimentBlocks.SAPPHIRE_ORE)
-                    .add(AscentExperimentBlocks.DEEPSLATE_SAPPHIRE_ORE);
+                    .add(AscentExperimentBlocks.DEEPSLATE_SAPPHIRE_ORE)
+                    .add(AscentExperimentBlocks.BLACKSTONE_RUTILE_ORE)
+                    .add(AscentExperimentBlocks.BASALT_RUTILE_ORE)
+                    .add(AscentExperimentBlocks.BLOOD_RUTILE_BLOCK)
+                    .add(AscentExperimentBlocks.ASTER_STONE)
+                    .add(AscentExperimentBlocks.CHARTIUM_ORE)
+                    .add(AscentExperimentBlocks.RAW_CHARTIUM_BLOCK)
+                    .add(AscentExperimentBlocks.CHARTIUM_BLOCK)
+                    .add(AscentExperimentBlocks.CHARTIUM_BRICKS);
 
             getOrCreateTagBuilder(BlockTags.PICKAXE_MINEABLE)
                     .add(AscentExperimentBlocks.SAPPHIRE_BLOCK)
                     .add(AscentExperimentBlocks.SAPPHIRE_ORE)
-                    .add(AscentExperimentBlocks.DEEPSLATE_SAPPHIRE_ORE);
+                    .add(AscentExperimentBlocks.DEEPSLATE_SAPPHIRE_ORE)
+                    .add(AscentExperimentBlocks.BLACKSTONE_RUTILE_ORE)
+                    .add(AscentExperimentBlocks.BASALT_RUTILE_ORE)
+                    .add(AscentExperimentBlocks.BLOOD_RUTILE_BLOCK)
+                    .add(AscentExperimentBlocks.ASTER_STONE)
+                    .add(AscentExperimentBlocks.CHARTIUM_ORE)
+                    .add(AscentExperimentBlocks.RAW_CHARTIUM_BLOCK)
+                    .add(AscentExperimentBlocks.CHARTIUM_BLOCK)
+                    .add(AscentExperimentBlocks.CHARTIUM_BRICKS);
         }
     }
 
@@ -41,18 +57,48 @@ public class AscentExperimentTagGenerators {
 
         @Override
         protected void configure(RegistryWrapper.WrapperLookup lookup) {
-            getOrCreateTagBuilder(AscentExperiment.REPAIRS_SAPPHIRE_ARMOR).add(AscentExperimentItems.SAPPHIRE);
+            getOrCreateTagBuilder(AscentExperiment.REPAIRS_SAPPHIRE_ARMOR)
+                .add(AscentExperimentItems.SAPPHIRE)
+                .add(AscentExperimentItems.BLOOD_RUTILE)
+                .add(AscentExperimentItems.CHARTIUM_INGOT);
 
-            getOrCreateTagBuilder(ItemTags.AXES).add(AscentExperimentItems.SAPPHIRE_AXE);
-            getOrCreateTagBuilder(ItemTags.HOES).add(AscentExperimentItems.SAPPHIRE_HOE);
-            getOrCreateTagBuilder(ItemTags.PICKAXES).add(AscentExperimentItems.SAPPHIRE_PICKAXE);
-            getOrCreateTagBuilder(ItemTags.SHOVELS).add(AscentExperimentItems.SAPPHIRE_SHOVEL);
-            getOrCreateTagBuilder(ItemTags.SWORDS).add(AscentExperimentItems.SAPPHIRE_SWORD);
+            getOrCreateTagBuilder(ItemTags.AXES)
+                .add(AscentExperimentItems.SAPPHIRE_AXE)
+                .add(AscentExperimentItems.BLOOD_RUTILE_AXE)
+                .add(AscentExperimentItems.CHARTIUM_AXE);
+            getOrCreateTagBuilder(ItemTags.HOES)
+                .add(AscentExperimentItems.SAPPHIRE_HOE)
+                .add(AscentExperimentItems.BLOOD_RUTILE_HOE)
+                .add(AscentExperimentItems.CHARTIUM_HOE);
+            getOrCreateTagBuilder(ItemTags.PICKAXES)
+                .add(AscentExperimentItems.SAPPHIRE_PICKAXE)
+                .add(AscentExperimentItems.BLOOD_RUTILE_PICKAXE)
+                .add(AscentExperimentItems.CHARTIUM_PICKAXE);
+            getOrCreateTagBuilder(ItemTags.SHOVELS)
+                .add(AscentExperimentItems.SAPPHIRE_SHOVEL)
+                .add(AscentExperimentItems.BLOOD_RUTILE_SHOVEL)
+                .add(AscentExperimentItems.CHARTIUM_SHOVEL);
+            getOrCreateTagBuilder(ItemTags.SWORDS)
+                .add(AscentExperimentItems.SAPPHIRE_SWORD)
+                .add(AscentExperimentItems.BLOOD_RUTILE_SWORD)
+                .add(AscentExperimentItems.CHARTIUM_SWORD);
 
-            getOrCreateTagBuilder(ItemTags.HEAD_ARMOR).add(AscentExperimentItems.SAPPHIRE_HELMET);
-            getOrCreateTagBuilder(ItemTags.CHEST_ARMOR).add(AscentExperimentItems.SAPPHIRE_CHESTPLATE);
-            getOrCreateTagBuilder(ItemTags.LEG_ARMOR).add(AscentExperimentItems.SAPPHIRE_LEGGINGS);
-            getOrCreateTagBuilder(ItemTags.FOOT_ARMOR).add(AscentExperimentItems.SAPPHIRE_BOOTS);
+            getOrCreateTagBuilder(ItemTags.HEAD_ARMOR)
+                .add(AscentExperimentItems.SAPPHIRE_HELMET)
+                .add(AscentExperimentItems.BLOOD_RUTILE_HELMET)
+                .add(AscentExperimentItems.CHARTIUM_HELMET);
+            getOrCreateTagBuilder(ItemTags.CHEST_ARMOR)
+                .add(AscentExperimentItems.SAPPHIRE_CHESTPLATE)
+                .add(AscentExperimentItems.BLOOD_RUTILE_CHESTPLATE)
+                .add(AscentExperimentItems.CHARTIUM_CHESTPLATE);
+            getOrCreateTagBuilder(ItemTags.LEG_ARMOR)
+                .add(AscentExperimentItems.SAPPHIRE_LEGGINGS)
+                .add(AscentExperimentItems.BLOOD_RUTILE_LEGGINGS)
+                .add(AscentExperimentItems.CHARTIUM_LEGGINGS);
+            getOrCreateTagBuilder(ItemTags.FOOT_ARMOR)
+                .add(AscentExperimentItems.SAPPHIRE_BOOTS)
+                .add(AscentExperimentItems.BLOOD_RUTILE_BOOTS)
+                .add(AscentExperimentItems.CHARTIUM_BOOTS);
         }
     }
 }
